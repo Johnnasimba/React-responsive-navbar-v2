d9a0d94fbc2bfa98ea53c8f1756fa0a047b025a4
JOHN NASIMBA <nasimba4john@gmail.com>
2021-09-02 18:47:47 +0200

    Adding menu items to navbar component

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import MenuList from './menu-list';
 
 const Navbar  = () => {
+    const menuList = MenuList.map(({title, url}, index) => {
+        return (
+            <li key = {index}>
+                <a href={url}> {title} </a>
+            </li>
+        )
+    })
     return (
-        <div className="navbar">
-            Nav bar
-        </div>
+        <nav className="navbar">
+            <div className="logo">Fitness<font>Hub</font></div>
+            <ul className="menu-list">
+                {menuList}
+            </ul>
+        </nav>
     )
 }
 
